2052b7634ea1e91112a935b4a73954a29aead207
Maira509 <bsef18a509@pucit.edu.pk>
2022-01-15 12:47:30 +0500

    Share on Whatsapp

--- app/src/main/java/com/example/activity1/MainActivity2.java
+++ app/src/main/java/com/example/activity1/MainActivity2.java
@@ -2,7 +2,10 @@ package com.example.activity1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Button inform = (Button) findViewById(R.id.inform);
+        inform.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.setPackage("com.whatsapp");
+                intent.putExtra(Intent.EXTRA_TEXT, "Emergency! It's me Maira Malik. "+"\n I am in trouble. "+"My location  is : ");
+                intent.putExtra("Kainy 2", "3367383656" + "@s.whatsapp.net");
+                startActivity(intent);
+
+            }
+        }
+        );
     }
 }
